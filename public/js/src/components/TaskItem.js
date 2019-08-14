c258c1fc9ad124d11e3b3253c190bbb1885cf96c
dbobb220 <david.bobb.220@gmail.com>
2019-08-14 14:00:12 -0500

    components created and copy/paste raw code

---
+++ public/js/src/components/TaskItem.js
@@ -0,0 +1,4 @@
+<a href="#" className="list-group-item">
+    <span className="badge">just now</span>
+    <i className="fa fa-fw fa-calendar"></i> Calendar updated
+</a>
---
+++ public/js/src/components/TasksPanel.js
@@ -0,0 +1,19 @@
+<div className="panel panel-default">
+    <div className="panel-heading">
+        <h3 className="panel-title"><i className="fa fa-clock-o fa-fw"></i> Tasks Panel</h3>
+    </div>
+    <div className="panel-body">
+        <div className="list-group">
+            {/*   <TaskItem>    */}
+            <a href="#" className="list-group-item">
+                <span className="badge">just now</span>
+                <i className="fa fa-fw fa-calendar"></i> Calendar updated
+            </a>
+            {/*   </TaskItem>    */}
+            
+        </div>
+        <div className="text-right">
+            <a href="#">View All Activity <i className="fa fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
